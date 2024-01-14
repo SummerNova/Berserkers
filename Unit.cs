030bc5ae219a0e6ec85ba64ad5a63f5e5b15d919
SummerNova <137454229+SummerNova@users.noreply.github.com>
2024-01-14 02:27:11 +0200

    position functionality made

--- Unit.cs
+++ Unit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Berserkers
 {
@@ -13,7 +14,9 @@ namespace Berserkers
         protected virtual int HP { get; set; } = 100;
         protected virtual string Name { get; set; }
         protected virtual Races Race { get; set; }
-
+        protected virtual int Speed { get; set; } = 1;
+        protected virtual Vector2 Position { get; set; } = new Vector2(0,0);
+        
         public virtual void Attack(Unit otherUnit)
         {
             otherUnit.Defend(this);
@@ -30,15 +33,89 @@ namespace Berserkers
         {
             return HP > 0;
         }
+
+        public Vector2 getPosition() { return Position; }
+
+        public virtual void move(Vector2 target)
+        {
+            float distanceX = target.X - Position.X;
+            float distanceY = target.Y - Position.Y;    
+
+            if (Math.Abs(distanceX) > Speed)
+            {
+                distanceX = Speed*Math.Sign(distanceX);
+            }
+            if (Math.Abs(distanceY) > Speed)
+            {
+                distanceY = Speed * Math.Sign(distanceY);
+            }
+
+            Position += new Vector2(distanceX, distanceY);
+        }
+
+        public float distance(Unit otherUnit)
+        {
+            return Math.Max(Math.Abs(otherUnit.Position.X - Position.X), Math.Abs(otherUnit.Position.Y - Position.Y));
+        }
     }
 
     abstract class RangedUnit : Unit
     {
+        protected virtual float Range { get; set; } = 3;
+
+
+        public override void Attack(Unit otherUnit)
+        {
+            if (distance(otherUnit) <= Range)
+            {
+                base.Attack(otherUnit);
+            }
+            else
+            {
+                move(AdjustTargetForRange(otherUnit));
+                if (distance(otherUnit) <= Range)
+                {
+                    base.Attack(otherUnit);
+                }
+            }
+        }
+        protected Vector2 AdjustTargetForRange(Unit otherUnit)
+        {
+            float TargetX = otherUnit.getPosition().X;
+            float TargetY = otherUnit.getPosition().Y;
+
+            if (Math.Abs(TargetX-Position.X) > Range)
+            {
+                TargetX = TargetX + Range*Math.Sign(TargetX-Position.X);
+            }
+            else TargetX = Position.X;
+            if (Math.Abs(TargetY-Position.Y) > Range)
+            {
+                TargetY = TargetY + Range * Math.Sign(TargetY - Position.Y);
+            }
+            else TargetY = Position.Y;
+
+            return new Vector2(TargetX, TargetY);
+        }
 
     }
 
     abstract class MeleeUnit : Unit
     {
-
+        public override void Attack(Unit otherUnit)
+        {
+            if (distance(otherUnit) <= 1)
+            {
+                base.Attack(otherUnit);
+            }
+            else
+            {
+                move(otherUnit.getPosition());
+                if (distance(otherUnit) <= 1)
+                {
+                    base.Attack(otherUnit);
+                }
+            }
+        }
     }
 }
